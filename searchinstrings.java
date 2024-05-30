fb2089a368edd0b73a74221222fccc442deb02fb
divyansh48 <166692812+divyansh48@users.noreply.github.com>
2024-05-30 07:23:00 +0530

    Add files via upload

---
+++ searchinstrings.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class searchinstrings {
+    public static void main(String[] args) {
+        String name = "Divyansh";
+        char target = 'a';
+
+        System.out.println(search(name , target));
+    }
+
+    static boolean search(String str, char target) {
+        if (str.length()==0){
+            return false;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            if (target==str.charAt(i)){
+                return true;
+            }
+
+        }
+        return false;
+    }
+}
